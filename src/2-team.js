4ecd287a945574579da473d933c84f6f0e1dae3a
Sanaa-Uzuri Robotham <sanaauzurirm@gmail.com>
2025-12-16 14:53:14 -0500

    Finished 1-media.js

--- src/2-team.js
+++ src/2-team.js
@@ -2,12 +2,106 @@
 // Implement your Player and Team classes below
 
 class Player {
-
+    constructor(name, position, jerseyNumber) {
+        this.name = name
+        this.position = position
+        this.jerseyNumber = jerseyNumber
+    }
 }
+const player1 = new Player("LeBron James", "Forward", 23);
+const player2 = new Player("Stephen Curry", "Guard", 30);
+const player3 = new Player("Kevin Durant", "Forward", 35);
+
+console.log(player1); // Player { name: "LeBron James", position: "Forward", jerseyNumber: 23 }
+console.log(player2); // Player { name: "Stephen Curry", position: "Guard", jerseyNumber: 30 }
+console.log(player3); // Player { name: "Kevin Durant", position: "Forward", jerseyNumber: 35 }
 
 class Team {
+    #starters = []
+    #bench = []
+    constructor(name) {
+        this.name = name
+    }
+    get starters() {
+        return this.#starters
+    }
+    get bench() {
+        return this.#bench
+    }
+    addBenchPlayer(player) {
+        return this.#bench.push(player)
+    }
+    getPlayerCount() {
+        return this.#starters.length + this.#bench.length
+    }
+    moveToBench(name) {
+        let currentPlayer = this.starters.find((player) => player.name === name)
+        if (!currentPlayer) {
+            return false
+        }
+        let index = this.#starters.indexOf(currentPlayer)
+        this.#starters.splice(index, 1)
+        this.#bench.push(currentPlayer)
+        return true
+    }
+    moveToStarters(name) {
 
+    }
 }
+const player1 = new Player("LeBron James", "Forward", 23);
+const player2 = new Player("Stephen Curry", "Guard", 30);
+const player3 = new Player("Kevin Durant", "Forward", 35);
+const player4 = new Player("Anthony Davis", "Center", 3);
+
+const team = new Team("All Stars");
+console.log(team); // Team { name: "All Stars" }
+
+// 1. Adding players to the bench
+team.addBenchPlayer(player1);
+team.addBenchPlayer(player2);
+team.addBenchPlayer(player3);
+team.addBenchPlayer(player4);
+
+console.log(team.getBench());
+// OR if you use the get syntax:
+console.log(team.bench);
+/*
+[
+  Player { name: "LeBron James", position: "Forward", jerseyNumber: 23 },
+  Player { name: "Stephen Curry", position: "Guard", jerseyNumber: 30 },
+  Player { name: "Kevin Durant", position: "Forward", jerseyNumber: 35 },
+  Player { name: "Anthony Davis", position: "Center", jerseyNumber: 3 }
+]
+*/
+console.log(team.getStarters()); // []
+
+// 2. Getting total player count
+console.log(team.getPlayerCount()); // 4
+
+// 3. Moving players to starters
+console.log(team.moveToStarters("LeBron James")); // true
+console.log(team.moveToStarters("Stephen Curry")); // true
+console.log(team.moveToStarters("Michael Jordan")); // false (not on the team)
+
+console.log(team.getStarters());
+/*
+[
+  Player { name: "LeBron James", position: "Forward", jerseyNumber: 23 },
+  Player { name: "Stephen Curry", position: "Guard", jerseyNumber: 30 }
+]
+*/
+console.log(team.getBench());
+/*
+[
+  Player { name: "Kevin Durant", position: "Forward", jerseyNumber: 35 },
+  Player { name: "Anthony Davis", position: "Center", jerseyNumber: 3 }
+]
+*/
+
+// 4. Moving a player back to the bench
+console.log(team.moveToBench("Stephen Curry")); // true
+console.log(team.getStarters().length); // 1
+console.log(team.getBench().length); // 3
 
 module.exports = { Player, Team };
 
